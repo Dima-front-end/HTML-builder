3cf5fc67ff88d74127fb10b695e4822a1a061137
Goroshko Dima <dimon.offical666@gmail.com>
2023-05-08 21:05:54 +0300

    feat: 04-copy-directory completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,86 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+  const filesDir = path.join(__dirname, 'files');
+  const copyDir = path.join(__dirname, 'files-copy');
+
+  // Создание папки files-copy
+  fs.mkdir(copyDir, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Чтение содержимого папки files
+    fs.readdir(filesDir, (err, files) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      // Копирование файлов
+      for (const file of files) {
+        const srcFile = path.join(filesDir, file);
+        const destFile = path.join(copyDir, file);
+
+        fs.stat(srcFile, (err, fileStat) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          if (fileStat.isFile()) {
+            fs.copyFile(srcFile, destFile, (err) => {
+              if (err) {
+                console.error(err);
+              }
+            });
+          } else if (fileStat.isDirectory()) {
+            copyDirRecursive(srcFile, destFile);
+          }
+        });
+      }
+    });
+  });
+}
+
+function copyDirRecursive(src, dest) {
+  fs.mkdir(dest, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    fs.readdir(src, (err, files) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      for (const file of files) {
+        const srcFile = path.join(src, file);
+        const destFile = path.join(dest, file);
+
+        fs.stat(srcFile, (err, fileStat) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          if (fileStat.isFile()) {
+            fs.copyFile(srcFile, destFile, (err) => {
+              if (err) {
+                console.error(err);
+              }
+            });
+          } else if (fileStat.isDirectory()) {
+            copyDirRecursive(srcFile, destFile);
+          }
+        });
+      }
+    });
+  });
+}
+
+copyDir();
